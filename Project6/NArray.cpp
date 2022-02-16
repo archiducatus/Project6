587e9d714f58ce2be0cd21b1b4f1f7bb9793bc63
권우 <kwoo827@gmail.com>
2022-02-16 17:48:51 +0900

    delete

--- Project6/NArray.cpp
+++ Project6/NArray.cpp
@@ -15,6 +15,41 @@ public:
 	Array(int dim, int* array_size) : dim(dim) {
 		size = new int[dim];
 		for (int i = 0; i < dim; i++) size[i] = array_size[i];
+
+		top = new Address;
+		top->level = 0;
+
+		initialize_address(top);
+	}
+
+	Array(const Array& arr) : dim(arr.dim) {
+		size = new int[dim];
+		for (int i = 0; i < dim; i++) size[i] = arr.size[i];
+
+		top = new Address;
+		top->level = 0;
+
+		initialize_address(top);
+		copy_address(top, arr.top);
+	}
+
+	~Array() {
+		delete_address(top);
+		delete[] size;
+	}
+
+	void copy_address(Address* dst, Address* src) {
+		if (dst->level == dim - 1) {
+			for (int i = 0; i < size[dst->level]; i++)
+				static_cast<int*>(dst->next)[i] = static_cast<int*>(src->next)[i];
+			return;
+		}
+
+		for (int i = 0; i != size[dst->level]; i++) {
+			Address* new_dst = static_cast<Address*>(dst->next) + i;
+			Address* new_src = static_cast<Address*>(src->next) + i;
+			copy_address(new_dst, new_src);
+		}
 	}
 
 	void initialize_address(Address* current) {
@@ -30,6 +65,19 @@ public:
 
 			initialize_address(static_cast<Address*>(current->next) + i);
 		}
+	}
+
+	void delete_address(Address* current) {
+		if (!current) return;
+		for (int i = 0; current->level < dim - 1 && i < size[current->level]; i++) {
+			delete_address(static_cast<Address*>(current->next) + i);
+		}
+
+		if (current->level == dim - 1) {
+			delete[] static_cast<int*>(current->next);
+		}
+
+		delete[] static_cast<Address*>(current->next);
 	}
 };
 
